6a343b7a327f58907819fd0c5be134efd759f618
有澤孝輝 <teruuuuun72@gmail.com>
2021-01-09 14:20:58 +0900

    [Add]3-9

---
+++ JavaWcp/src/Sukkiri3_9.java
@@ -0,0 +1,12 @@
+
+public class Sukkiri3_9 {
+	public static void main(String[] args) {
+		for (int a = 1; a < 10; a++) {
+			for (int b = 1; b < 10; b++) {
+				System.out.print(a * b);
+				System.out.print(" ");
+			}
+			System.out.println("");
+		}
+	}
+}
