c84a2d19952ab9d9affca5dfb9230d78976e188b
有澤孝輝 <teruuuuun72@gmail.com>
2021-01-10 12:36:04 +0900

    [Add]practice4

---
+++ JavaWcp/src/Practice4.java
@@ -0,0 +1,18 @@
+/*
+ * 4-1
+ * 1. int[] points = new int[4];
+ * 2. double[] weights = new double[5];
+ * 3. boolean[] answers = new boolean[3];
+ * 4. String[] names = new Strings[3];
+ */
+public class Practice4 {
+	public static void main(String[] args) {
+		int[] moneyList = new int[] {121902, 8302, 55100};
+		for ( int  a = 0; a < moneyList.length; a++) {
+			System.out.println(moneyList[a]);
+		}
+		for (int value : moneyList) {
+			System.out.println(value);
+		}
+	}
+}
