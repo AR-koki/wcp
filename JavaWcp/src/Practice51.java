e6812d61f643e0e998bc0f9f094c841bbb45a9b7
有澤孝輝 <teruuuuun72@gmail.com>
2021-01-24 13:57:11 +0900

    sukkiri

---
+++ JavaWcp/src/Practice51.java
@@ -0,0 +1,12 @@
+
+public class Practice51 {
+	public static void introduceOneself(String n, int a, double h, char s) {
+		System.out.println("私の名前は" + n + "です");
+		System.out.println("年齢は" + a + "です");
+		System.out.println("身長は" + h + "です");
+		System.out.println("干支は" + s + "です");
+	}
+	public static void main(String[] args) {
+		introduceOneself("有澤",27,173.5,'鳥');
+	}
+}
