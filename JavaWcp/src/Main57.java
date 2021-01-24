8802209ec39b8399d6b7eba5dfcdc8af120f23d7
有澤孝輝 <teruuuuun72@gmail.com>
2021-01-24 13:19:15 +0900

    sukkiri

--- JavaWcp/src/Main57.java
+++ JavaWcp/src/Main57.java
@@ -1,11 +1,11 @@
 
 public class Main57 {
-	public static int add(int x, int y) {
-		int ans = x + y;
-		return ans;
-	}
 	public static void main(String[] args) {
 		int ans = add(100,10);
 		System.out.println("100 + 10 =" + ans);
+	}
+	public static int add(int x, int y) {
+		int ans = x + y;
+		return ans;
 	}
 }
